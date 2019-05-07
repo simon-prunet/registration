8a8b491d18b20adb14ade94b9f80067ed50f5b13
Simon Prunet <prunet@gmail.com>
2019-05-06 17:32:11 -1000

    compute_offset_phase method gives a phase matrix, that when multiplied by the 2D Fourier transform of an image translates it by dx,dy
    compute_d_offset_phase method gives a particular partial derivative of the above phase term, of order (i,j)
    e.g. i,j=2,0 is a second derivative with respect to the x offset, around offset position dx,dy
    
    compute_cross method computes the phase correlation between (FFT of) images A and B.
    It is maximized when image A has been translated w.r.t image B by dx,dy
    
    These methods will be bricks used in the sub-pixel optimization (probably a Newton method)

--- register/register.py
+++ register/register.py
@@ -41,3 +41,48 @@ class Offset:
             dy -= ny
 
         return cross
+
+    def compute_offset_phase(self,(dx,dy)):
+        '''
+        :param (dx,dy): tuple of offset values (float)
+        :return: phase term corresponding to a translation by dx,dy (in pixel units)
+        '''
+
+        (nx,ny) = self.f_ref_image.shape
+        x_freq = np.fft.fftfreq(nx)
+        y_freq = np.fft.fftfreq(ny)
+        x_phase = np.exp(-1j * 2*np.pi * dx * x_freq)
+        y_phase = np.exp(-1j * 2*np.pi * dy * y_freq)
+
+        return np.outer(x_phase,y_phase)
+
+    def compute_d_offset_phase(self,(dx,dy),(i,j)):
+        '''
+        :param (dx,dy): tuple of offset values(float)
+        :param (i,j): tuple of partial derivative indices
+        :return: partial derivative of phase term at reference translation dx,dy
+        '''
+
+        phi = self.compute_offset_phase((dx,dy))
+        nx,ny = self.f_ref_image.shape
+        x_freq = np.fft.fftfreq(nx)
+        y_freq = np.fft.fftfreq(ny)
+        A = np.diag((1j * 2*np.pi* x_freq)**i)
+        B = np.diag((1j * 2*np.pi *y_freq)**j)
+        return np.dot(A,np.dot(phi,B))
+
+    def compute_cross(self,fimA,fimB,(dx,dy)):
+        '''
+        Translated image is the first one
+        :param fimA: Translated image 2D Fourier transform
+        :param fimB: Reference image 2D Fourier transform
+        :return: phase correlation at given offset dx,dy.
+                 Should be maximal when dx,dy corresponds to translation.
+        '''
+        nfa = np.sum(fimA*fimA.conj())
+        nfb = np.sum(fimB*fimB.conj())
+        phi = self.compute_offset_phase((-dx,-dy))
+        return np.sum(fimA*fimB.conj()*phi).real / np.sqrt((nfa*nfb).real)
+
+
+
